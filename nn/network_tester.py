38632eb3bb108a347f76ab02b8ab816bc6519e5d
matthew kirchhof <matthew kirchhof>
2017-04-21 14:06:54 -0400

    Added final tester class file

---
+++ nn/network_tester.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 21, 2017
+
+@author: Matth
+'''
+
+class network_tester():
+    
+    def __init__(self, networkData):
+        nd = networkData
+        
+        print ("Tester instantiated.")
+        
+        
+    def trainNetwork(self):
+        print ("Testing Network!")
