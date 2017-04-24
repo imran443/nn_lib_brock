25f69079c80c29e10865e0a2cd3392785e2ae93e
matthew kirchhof <matthew kirchhof>
2017-04-24 10:12:22 -0400

    Merged backprop in

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ Created on Apr 23, 2017
 from setuptools import setup
 
 setup(name='brocknet',
-      version='0.20',
+      version='0.21',
       description='Brock University Python neural network library',
       url='',
       author='Matthew Kirchhof, Imran Qureshi',
-      author_email='mk12ld@example.com',
+      author_email='mk12ld@example.com, iq13rr@brocku.ca',
       license='MIT',
       packages=['brocknet'],
       zip_safe=False)
